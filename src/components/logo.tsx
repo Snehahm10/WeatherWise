50885fc67f6b61cc5348f738c74fad6efd252d7a
Sneha Hm <snehahanjgimath@gmail.com>
2025-09-01 19:46:19 +0000

    reduce to 2xl

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,13 +1,12 @@
-
 import { Sun } from 'lucide-react';
 import React from 'react';
 
 export function Logo() {
   return (
     <div className="flex items-center gap-2">
-      <span className="text-xl font-bold text-foreground tracking-wider font-headline">WeatherWise</span>
+      <span className="text-2xl font-bold text-foreground tracking-wider font-headline">WeatherWise</span>
       <div className="p-1 bg-gradient-to-br from-blue-500 to-blue-600 rounded-md shadow-md">
-        <Sun className="h-5 w-5 text-white" />
+        <Sun className="h-6 w-6 text-white" />
       </div>
     </div>
   );
